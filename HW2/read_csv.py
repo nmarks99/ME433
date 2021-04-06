c3ea78d3862071e06aa94bad8ce6fbd8092a4143
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-06 10:16:44 -0500

    Update read_csv.py

--- HW2/read_csv.py
+++ HW2/read_csv.py
@@ -8,27 +8,42 @@ sigPath = [
     r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigD.csv'
     ]
 
+# Creates some lists to store the data
 tA = []; tB = []; tC = []; tD = []
 sigA = []; sigB = []; sigC = []; sigD = []
+
+# Open each CSV file, read it, and append to the appropriate list
+# Probably a more efficient way of doing this but this works fine
 with open(sigPath[0]) as dataA:
     for row in dataA:
         row = row.split(',')
         tA.append(float(row[0]))
         sigA.append(float(row[1]))
-
 with open(sigPath[1]) as dataB:
     for row in dataB:
         row = row.split(',')
         tB.append(float(row[0]))
         sigB.append(float(row[1]))
+with open(sigPath[2]) as dataC:
+    for row in dataC:
+        row = row.split(',')
+        tC.append(float(row[0]))
+        sigC.append(float(row[1]))
+with open(sigPath[3]) as dataD:
+    for row in dataD:
+        row = row.split(',')
+        tD.append(float(row[0]))
+        sigD.append(float(row[1]))
+
+t = [tA,tB,tC,tD]
+sigs = [sigA,sigB,sigC,sigD]
 
 # sample rate = number of data points / total time of samples
-samp_rateA = len(sigA)/tA[-1]
-print('Sample Rate A = %f' % samp_rateA)
-
-samp_rateB = len(sigB)/tB[-1]
-print('Sample Rate B = %f' % samp_rateB)
-# Appears that the same rate is the same for all 4 signals ~= 10000
+samp_rates = []
+for i in range(len(sigs)):
+    sr = len(sigs[i])/t[i][-1]
+    samp_rates.append(sr)
+print(samp_rates)
 
 # plt.figure()
 # plt.plot(tA,sigA,'-b')
