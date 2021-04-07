8866f6680b38b4bb505bca531965e3155da34179
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-07 12:39:51 -0500

    Update LPF_MAF.py

--- HW2_DSP/LPF_MAF.py
+++ HW2_DSP/LPF_MAF.py
@@ -75,4 +75,69 @@ for i in range(len(sigs)):
     ax.grid()
     plt.show()
 
+# Generate FFT of the unfiltered data
+samp_rates = []
+for i in range(len(sigs)):
+    sr = len(sigs[i])/ts[i][-1]
+    samp_rates.append(sr) 
 
+t_uf = []
+frq_uf = []
+Y_uf = []
+for i in range(len(ts)):
+    dt = 1/len(ts[i])
+    t_temp = np.arange(0.0, 1.0, dt)
+    
+    y_temp = sigs[i]    # Unfiltered data
+    Fs = samp_rates[i]
+    n = len(y_temp)
+    k = np.arange(n)
+    T = n/Fs
+    frq_temp = k/T
+    frq_temp = frq_temp[range(int(n/2))]  # one side frequency range
+    Y_temp = np.fft.fft(y_temp)/n         # fft computing and normalization
+    Y_temp = Y_temp[range(int(n/2))]
+
+    t_uf.append(t_temp)
+    frq_uf.append(frq_temp)
+    Y_uf.append(Y_temp)
+
+# Generate FFT for the filtered data
+t_f = []
+frq_f = []
+Y_f = []
+for i in range(len(ts)):
+    dt = 1/len(ts[i])
+    t_temp = np.arange(0.0, 1.0, dt)
+    
+    y_temp = sigs_mav[i]    # Filtered data
+    Fs = samp_rates[i]
+    n = len(y_temp)
+    k = np.arange(n)
+    T = n/Fs
+    frq_temp = k/T
+    frq_temp = frq_temp[range(int(n/2))]  # one side frequency range
+    Y_temp = np.fft.fft(y_temp)/n         # fft computing and normalization
+    Y_temp = Y_temp[range(int(n/2))]
+
+    t_f.append(t_temp)
+    frq_f.append(frq_temp)
+    Y_f.append(Y_temp)
+
+# Modify values so dimensions agree
+# for i in range(len(t_f)):
+#     t_f[i] = t_f[i][num_avg[i]-1:]
+#     f_uf[i] = f_uf[i][num_avg[i]-1:]
+#     Y_uf[i] = Y_uf[i][num_avg[i]-1:]
+
+# Plot them both on the same axis
+ttl = ['FFT A','FFT B','FFT C','FFT D']
+
+for i in range(len(sigs)):
+    fig,ax = plt.subplots()
+    ax.loglog(frq_uf[i],abs(Y_uf[i]),'-k',label='Unfiltered')
+    ax.loglog(frq_f[i],abs(Y_f[i]),'-r',label='Filtered')
+    ax.legend()
+    ax.set(xlabel='Time (s)', ylabel='Y|Y(freq)|',title=ttl[i])
+    ax.grid()
+    plt.show()
