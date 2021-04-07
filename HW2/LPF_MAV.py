176f27f6568ba2d0a2695049a6573fa04c92ca82
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-06 22:20:33 -0500

    Create LPF_MAV.py
    Low-pass filter using a moving average

---
+++ HW2/LPF_MAV.py
@@ -0,0 +1,54 @@
+from matplotlib import pyplot as plt 
+import numpy as np
+
+sigPath = [
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigA.csv',
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigA.csv',
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigC.csv',
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigD.csv'
+    ]
+
+# Creates some lists to store the data
+tA = []; tB = []; tC = []; tD = []
+sigA = []; sigB = []; sigC = []; sigD = []
+
+# Open each CSV file, read it, and append to the appropriate list
+# Probably a more efficient way of doing this but this works fine
+with open(sigPath[0]) as dataA:
+    for row in dataA:
+        row = row.split(',')
+        tA.append(float(row[0]))
+        sigA.append(float(row[1]))
+with open(sigPath[1]) as dataB:
+    for row in dataB:
+        row = row.split(',')
+        tB.append(float(row[0]))
+        sigB.append(float(row[1]))
+with open(sigPath[2]) as dataC:
+    for row in dataC:
+        row = row.split(',')
+        tC.append(float(row[0]))
+        sigC.append(float(row[1]))
+with open(sigPath[3]) as dataD:
+    for row in dataD:
+        row = row.split(',')
+        tD.append(float(row[0]))
+        sigD.append(float(row[1]))
+
+# Store time and signal data in lists
+ts = [tA,tB,tC,tD]
+sigs = [sigA,sigB,sigC,sigD]
+sigs_ma5 = []
+
+# Moving average of the last 5 data points
+count = 0
+ma5 = 0
+for i in sigs[3]:
+    if count < 5:
+        ma5 = ma5 + i
+        count += 1
+    elif count == 5:
+        count = 0
+        ma5 = ma5/5
+        sigs_ma5.append(ma5)
+        ma5 = 0
