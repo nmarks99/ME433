e13abd5b1b7028dc5b088e7b3e027de9f9da26d3
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-07 14:52:20 -0500

    Update LPF_IIR.py

---
+++ HW2_DSP/LPF_IIR.py
@@ -0,0 +1,46 @@
+from matplotlib import pyplot as plt 
+import numpy as np
+
+sigPath = [
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigA.csv',
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigA.csv',
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigC.csv',
+    r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigD.csv'
+    ]
+
+# Creates some lists to store the data
+tA = []; tB = []; tC = []; tD = []
+sigA = []; sigB = []; sigC = []; sigD = []
+
+# Open each CSV file, read it, and append to the appropriate list
+# I could make this more concise/efficient but ehh maybe later
+with open(sigPath[0]) as dataA:
+    for row in dataA:
+        row = row.split(',')
+        tA.append(float(row[0]))
+        sigA.append(float(row[1]))
+with open(sigPath[1]) as dataB:
+    for row in dataB:
+        row = row.split(',')
+        tB.append(float(row[0]))
+        sigB.append(float(row[1]))
+with open(sigPath[2]) as dataC:
+    for row in dataC:
+        row = row.split(',')
+        tC.append(float(row[0]))
+        sigC.append(float(row[1]))
+with open(sigPath[3]) as dataD:
+    for row in dataD:
+        row = row.split(',')
+        tD.append(float(row[0]))
+        sigD.append(float(row[1]))
+
+# Store time and signal data in lists
+ts = [tA,tB,tC,tD]
+sigs = [sigA,sigB,sigC,sigD]
+
+iir = []
+A = 0.5
+B = 0.5
+for i in range(len(sigs)):
+    iir[i] = A*iir[i-1] + B*sigs[0][i]
