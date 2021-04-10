a1a16d30e9963c50bda210154ac1343c2f1b364f
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-09 19:59:48 -0500

    Update DSP_FFT.py

--- HW2_DSP/DSP_FFT.py
+++ HW2_DSP/DSP_FFT.py
@@ -8,36 +8,20 @@ sigPath = [
     r'C:\Users\nmark\OneDrive\Documents\ME433\Python\HW2\sigD.csv'
     ]
 
-# Creates some lists to store the data
-tA = []; tB = []; tC = []; tD = []
-sigA = []; sigB = []; sigC = []; sigD = []
-
 # Open each CSV file, read it, and append to the appropriate list
-# Probably a more efficient way of doing this but this works fine
-with open(sigPath[0]) as dataA:
-    for row in dataA:
-        row = row.split(',')
-        tA.append(float(row[0]))
-        sigA.append(float(row[1]))
-with open(sigPath[1]) as dataB:
-    for row in dataB:
-        row = row.split(',')
-        tB.append(float(row[0]))
-        sigB.append(float(row[1]))
-with open(sigPath[2]) as dataC:
-    for row in dataC:
-        row = row.split(',')
-        tC.append(float(row[0]))
-        sigC.append(float(row[1]))
-with open(sigPath[3]) as dataD:
-    for row in dataD:
-        row = row.split(',')
-        tD.append(float(row[0]))
-        sigD.append(float(row[1]))
-
-# Store time and signal data in lists
-ts = [tA,tB,tC,tD]
-sigs = [sigA,sigB,sigC,sigD]
+sigs = []
+ts = []
+for i in range(len(sigPath)):
+    temp_sig = []
+    temp_t = []
+    with open(sigPath[i]) as data:
+        for row in data:
+            row = row.split(',')
+            temp_t.append(float(row[0]))
+            temp_sig.append(float(row[1]))
+        else:
+            sigs.append(temp_sig)
+            ts.append(temp_t)
 
 # Calculate the sample rate for each data set
 samp_rates = []
