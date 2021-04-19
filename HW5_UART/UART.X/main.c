1387996277ea46ec304c6795b0b974a0d52fa60b
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-18 22:21:17 -0500

    Update main.c
    Added readUART and writeUART functions from ME333

--- HW5_UART/UART.X/main.c
+++ HW5_UART/UART.X/main.c
@@ -1,5 +1,6 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
+#include <stdio.h>       // Standard library
 
 // DEVCFG0
 #pragma config DEBUG = OFF  // disable debugging
@@ -36,7 +37,10 @@
 
 #define CORE_TICKS 24000000 // sysclk = 48MHz; CORE_TICKS = sysclk/2 = 24M
 
+/* Function prototypes */
 void delay(float waitTime);
+void readUART1(char * message, int maxLength);
+void writeUART1(const char * string);
 
 int main() {
 
@@ -73,18 +77,21 @@ int main() {
     // configure TX & RX pins as output & input pins
     U1STAbits.UTXEN = 1;
     U1STAbits.URXEN = 1;
-    // configure hardware flow control using RTS and CTS
-    U1MODEbits.UEN = 2;
-
+   
     // enable the uart
     U1MODEbits.ON = 1;
     
     __builtin_enable_interrupts();
     
     int bflag = 0;
+    char m[100];
+    
     while (1) {
+        
         if (!PORTBbits.RB4 & bflag == 0){
             bflag = 1;  // Set bflag to 1 after detecting a button press
+            sprintf(m,"Hello\r\n");
+            writeUART1(m);
         }
         if (bflag > 0){ // Pulse light twice after button press
             LATAbits.LATA4 = 1; // LED on
@@ -95,7 +102,7 @@ int main() {
             delay(0.5);
             LATAbits.LATA4 = 0;
             bflag = 0;  // Reset the flag
-        }   
+        }  
     }
 }
 
@@ -106,4 +113,41 @@ void delay(float waitTime){
     while(_CP0_GET_COUNT() < CORE_TICKS*waitTime){
         // Wait for "waitTime" seconds
     }
+}
+
+// Read from UART3
+// block other functions until you get a '\r' or '\n'
+// send the pointer to your char array and the number of elements in the array
+void readUART1(char * message, int maxLength) {
+  char data = 0;
+  int complete = 0, num_bytes = 0;
+  // loop until you get a '\r' or '\n'
+  while (!complete) {
+    if (U1STAbits.URXDA) { // if data is available
+      data = U1RXREG;      // read the data
+      if ((data == '\n') || (data == '\r')) {
+        complete = 1;
+      } else {
+        message[num_bytes] = data;
+        ++num_bytes;
+        // roll over if the array is too small
+        if (num_bytes >= maxLength) {
+          num_bytes = 0;
+        }
+      }
+    }
+  }
+  // end the string
+  message[num_bytes] = '\0';
+}
+
+// Write a character array using UART3
+void writeUART1(const char * string) {
+  while (*string != '\0') {
+    while (U1STAbits.UTXBF) {
+      ; // wait until tx buffer isn't full
+    }
+    U1TXREG = *string;
+    ++string;
+  }
 }
