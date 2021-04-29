09b608c921dcf6d13fea25680f6552e2ab133b12
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-29 13:47:49 -0500

    update main.c ; update NM32.h

--- HW7_SPI/SPI.X/NM32.h
+++ HW7_SPI/SPI.X/NM32.h
@@ -1,14 +1,14 @@
 #ifndef NM32__H__
 #define NM32__H__
 
-#include<xc.h>                      // Processor SFR definitions
-#include<sys/attribs.h>             // __ISR macro
+#include<xc.h>           // Processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
 #include <stdio.h>                  
 #include <stdlib.h>
 
 #define NM32_LED1 LATAbits.LATA4    // LED1
 #define NM32_USER PORTBbits.RB4     // User button
-#define CORE_TICKS 24000000 // sys clk is 48000000, core timer is half that
+#define CORE_TICKS 24000000         // sys clk is 48MHz, core timer is half 
 
 void NM32_Startup(void);
 void readUART1(char * message, int maxLength);
--- HW7_SPI/SPI.X/main.c
+++ HW7_SPI/SPI.X/main.c
@@ -1,4 +1,3 @@
-
 #include "NM32.h"
 #include "spi.h"
 #include "math.h"
@@ -7,17 +6,17 @@ unsigned short bit_manip(unsigned short p, unsigned short v, unsigned char c);
 
 int main(void){
     
-    NM32_Startup();
+    NM32_Startup(); 
     initSPI();
     /*Output SD01 (A1)
       Input SDI1 (B5)
       Clock SCK1 (pin 25) */
     
-    unsigned short vmax = 4096;
-    unsigned short v = 0;  // Voltage I want between 0 and 4096; short is 2 bytes
-    unsigned char c;       // Channel A(0) or B(1)
-    unsigned short p;
-    float delayTime = 1.0;
+    unsigned short vmax = 4096; // 12-bit DAC, range between 0 and 2^12 = 4096
+    unsigned short v = 0;       // Voltage between 0 and 4096;
+    unsigned char c;            // Channel A(0) or B(1)
+    unsigned short p;           // Final variable we will send to the DAC
+    float delayTime = 1.0;      // Delay between sending data
     
     while(1){
         
@@ -49,6 +48,10 @@ int main(void){
 }
 
 unsigned short bit_manip(unsigned short p, unsigned short v, unsigned char c){
+    
+    /* This function performs the necessary bit manipulation to write the
+     correct bits over SPI to the DAC and outputs the unsigned short which we 
+     will write using the spi_io() function */
     p = (c << 15);
     p = p|(0b111 << 12);
     p = v|p;
