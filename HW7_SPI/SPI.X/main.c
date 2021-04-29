8f344cc22e70666aca41decb3cf6811a239dfd25
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-28 20:14:40 -0500

    update main.c and testing etc.

--- HW7_SPI/SPI.X/main.c
+++ HW7_SPI/SPI.X/main.c
@@ -1,6 +1,7 @@
 
 #include "NM32.h"
 #include "spi.h"
+#include "math.h"
 #define CORE_TICKS 24000000 // sys clk is 48000000, core timer is half that
 
 int main(void){
@@ -11,23 +12,44 @@ int main(void){
       Input SDI1 (B5)
       Clock SCK1 (pin 25) */
     
-    unsigned char i = 0;
+    unsigned short vmax = 4096;
+    unsigned short v1 = 0;  // Voltage I want between 0 and 4096; short is 2 bytes
+    unsigned char c1;       // Channel A(0) or B(1)
+    unsigned short p1;
+    int f = 1;
+    // cslow; spi_io(p>>8);spi_io(p)); cshigh;
     while(1){
+        
+        if (f == 1){    // go up
+            v1 += 100;
+        }
+        else if (f == 0){   // go down
+            v1 -= 100;
+        }
+        if (v1 >= vmax){
+            v1 = vmax;
+            f = 0;
+        }
+        if (v1 <= 0){
+            v1 = vmax;
+            f = 1;
+        }
+        
+        c1 = 1;
+        p1 = (c1 << 15);
+        p1 = p1|(0b111 << 12);
+        p1 = v1|p1;
+
         // Write 1 bit over SPI
         LATAbits.LATA0 = 0; // Bring CS low
-        spi_io(i);          // Write the byte
+        spi_io(p1 >> 8);    
+        spi_io(p1);
         LATAbits.LATA0 = 1; // Bring CS high
-        
-        i++;
-        if (i == 100){
-            i = 0;
-        }
-    
+
         _CP0_SET_COUNT(0);
-        while( _CP0_GET_COUNT() < CORE_TICKS / 2 ){ 
+        while( _CP0_GET_COUNT() < CORE_TICKS / 8 ){ 
             // 1Hz delay
-        }     
+        }
     }
-    
     return 0;
 }
