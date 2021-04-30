b6786eb0823df50feec2e7d386cdb74fb92071eb
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-04-30 11:58:45 -0500

    create makeLookupTables.ipynb

--- HW7_SPI/SPI.X/main.c
+++ HW7_SPI/SPI.X/main.c
@@ -2,7 +2,7 @@
 #include "spi.h"
 #include "math.h"
 
-unsigned short bit_manip(unsigned short p, unsigned short v, unsigned char c);
+unsigned short bit_manip(unsigned short p, unsigned short v1, unsigned char c);
 
 int main(void){
     
@@ -13,28 +13,36 @@ int main(void){
       Clock SCK1 (pin 25) */
     
     unsigned short vmax = 4096; // 12-bit DAC, range between 0 and 2^12 = 4096
-    unsigned short v;       // Voltage between 0 and 4096;
+    unsigned short v1;          // Voltage between 0 and 4096;
     unsigned char c;            // Channel A(0) or B(1)
     unsigned short p;           // Final variable we will send to the DAC
-    float delayTime = 0.005;      // Delay between sending data
+    unsigned short ps;
+    float delayTime = 0.01;     // Delay between sending data (seconds)
+    
+    
+    unsigned short sinTable[37] ={ 1999, 2347, 2684, 2999, 3285, 3532, 3732, 3879, 3969, 4000,
+      3969, 3879, 3732, 3532, 3285, 3000, 2684, 2347, 2000, 1652, 1315, 1000, 714, 467, 267, 120,
+       30, 0, 30, 120, 267, 467, 714, 999, 1315, 1652, 1999}; 
+    
+    int len_st = sizeof(sinTable)/2;
+    int i;
     
     while(1){
         
-        //v1 =  -200; negative outputs max voltage ???
-        
-        for (v = 0; v < 4095; v++){
-            p = bit_manip(p,v,0);
+        for (i=1; i < len_st; i++){
+            ps = bit_manip(ps,sinTable[i],1);
             
             // Write 1 bit over SPI
             LATAbits.LATA0 = 0; // Bring CS low
-            spi_io(p >> 8);    
-            spi_io(p);
+            spi_io(ps >> 8);    
+            spi_io(ps);
             LATAbits.LATA0 = 1; // Bring CS high
-
-//            core_delay(delayTime);  // Delay for 1/2 sec
+            
+            core_delay(delayTime);
         }
-        for (v = 4095; v > 0; v-- ){
-            p = bit_manip(p,v,0);
+//        
+        for (v1 = 0; v1 < 4095; v1++){
+            p = bit_manip(p,v1,0);
             
             // Write 1 bit over SPI
             LATAbits.LATA0 = 0; // Bring CS low
@@ -42,20 +50,31 @@ int main(void){
             spi_io(p);
             LATAbits.LATA0 = 1; // Bring CS high
 
-//            core_delay(delayTime);  // Delay for 1/2 sec
+            core_delay(delayTime);  // Delay for 1/2 sec
+        }
+        for (v1 = 4095; v1 > 0; v1-- ){
+            p = bit_manip(p,v1,0);
+            
+            // Write 1 bit over SPI
+            LATAbits.LATA0 = 0; // Bring CS low
+            spi_io(p >> 8);    
+            spi_io(p);
+            LATAbits.LATA0 = 1; // Bring CS high
+
+            core_delay(delayTime);  // Delay for 1/2 sec
         }
     }
     return 0;
 }
 
-unsigned short bit_manip(unsigned short p, unsigned short v, unsigned char c){
+unsigned short bit_manip(unsigned short p, unsigned short v1, unsigned char c){
     
     /* This function performs the necessary bit manipulation to write the
      correct bits over SPI to the DAC and outputs the unsigned short which we 
      will write using the spi_io() function */
     p = (c << 15);
     p = p|(0b111 << 12);
-    p = v|p;
+    p = v1|p;
     
     return p;
 }
--- HW7_SPI/test.c
+++ HW7_SPI/test.c
@@ -4,27 +4,38 @@
 
 int main(void){
 
-    float vf[4096];
-    int len = sizeof(vf)/2;  // Short is 2 bytes
+     unsigned short sinTable[37] ={ 1999, 2347, 2684, 2999, 3285, 3532, 3732, 3879, 3969, 4000,
+      3969, 3879, 3732, 3532, 3285, 3000, 2684, 2347, 2000, 1652, 1315, 1000, 714, 467, 267, 120,
+       30, 0, 30, 120, 267, 467, 714, 999, 1315, 1652, 1999};
+
     int i;
-
-
-    // Make an array of values 1:4095
-    for (i=0; i <= len; i+=25){ 
-        vf[i] =  (float)i+1;
-        // printf("%.2f\n",vf[i]);
-        if (vf[i] > (4094-25) ){
-            break;
-        } 
+    for (i = 0; i < 37; i++){
+        printf("%hu\n",sinTable[i]);
     }
+    int lenst = sizeof(sinTable)/2;
+    printf("\nLength = %d",lenst);
 
-    float sinv[4096];
-    // Compute sin + 1 for all the values
-    for (i=0;i <= len; i++){
-        sinv[i] = (sin(vf[i]));
+    // float vf[4096];
+    // int len = sizeof(vf)/2;  // Short is 2 bytes
+    // int i;
+
+
+    // // Make an array of values 1:4095
+    // for (i=0; i <= len; i+=25){ 
+    //     vf[i] =  (float)i+1;
+    //     // printf("%.2f\n",vf[i]);
+    //     if (vf[i] > (4094-25) ){
+    //         break;
+    //     } 
+    // }
+
+    // float sinv[4096];
+    // // Compute sin + 1 for all the values
+    // for (i=0;i <= len; i++){
+    //     sinv[i] = (sin(vf[i]));
         
-    }
-    printf("%.5f\n",sin(sinv[100]));
+    // }
+    // printf("%.5f\n",sin(sinv[100]));
    
 
     return 0;
