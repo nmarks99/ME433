947bb65a600d4902df59b3cb7ea642b7b924d5ce
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-02 14:28:01 -0500

    moved folders

--- HW8_I2C/I2C.X/main.c
+++ HW8_I2C/I2C.X/main.c
@@ -9,17 +9,19 @@ int main(void){
     /* 
     Initialize pins:
      GPA7 is output, make all the A pins output, IODIRA = 0x00 
-     address = 0b01000000, register = 0x00, value = 0x00
+     register = 0x00, value = 0x00
     
-     GBB0 is input make all B pins input, IODIRB = 0xFF 
-     address = 0b01000001, register = 0x01, value = 0xFF
+     GBB0 is input, make all B pins input, IODIRB = 0xFF 
+     register = 0x01, value = 0xFF
+     
+     We have set A0 = A1 = A2 = 0, so the address to the IO expander is 
+     0b01000000 for writing
+     0b01000001 for reading
      */
     
-    setPin(0b01000000,0x00,0x00);   // Make A pins outputs
-//    setPin(0b01000001,0x01,0xFF); // Make B pins input?
-    
+    setPin(0b01000000,0x00,0x00);   // Make A pins output
+    setPin(0b01000000,0x01,0xFF);   // Make B pins input
     setPin(0b01000000,0x14,0xFF);   // Turn on A pins  
-    
     
     while(1){
         
