e12b5de9d5ab1e629536d10429d9507168bdff21
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-16 23:26:23 -0500

    update read and write address

--- HW11_IMU/IMU.X/main.c
+++ HW11_IMU/IMU.X/main.c
@@ -4,8 +4,20 @@
 int main(void){
     NM32_Startup(); // Initialize the PIC32
     i2c_master_setup(); // Initialize I2C
-
-
-
+    
+    char buff[20];
+    int f;
+    while(1){
+        
+        
+        f = readPin(IMU_WADD, IMU_WHOAMI);
+        sprintf(buff,"I am %d\r\n",f);
+        writeUART1(buff);
+        
+        NM32_LED1 = 1;
+        core_delay(0.1);
+        NM32_LED1 = 0;
+        core_delay(0.1);
+    }
     return 0;
 }
