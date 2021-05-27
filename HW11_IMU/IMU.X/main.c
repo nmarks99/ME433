1fb36d74d452705e59d09c6e690c00005f6544f3
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-27 13:55:20 -0500

    updatae

--- HW11_IMU/IMU.X/main.c
+++ HW11_IMU/IMU.X/main.c
@@ -22,18 +22,42 @@ int main(void){
 
         imu_read(IMU_OUT_TEMP_L,data,14); // read the IMU temp, gyro, and acceleration
         
+        if (data[5] > 0){
+            for(i = 0; i < data[5]/30;i++){
+                LCD_drawPixel(home+i,home,WHITE);
+            }
+        }  
+        else{
+            for(i = 0; i < abs(data[5])/30;i++){
+                LCD_drawPixel(home-i,home,WHITE);
+            } 
+        }
+
+        core_delay(0.01);
+
+        if (data[5] > 0){
+            for(i = 0; i < data[5]/30;i++){
+                LCD_drawPixel(home+i,home,BLUE);
+            }
+        }  
+        else{
+            for(i = 0; i < abs(data[5])/30;i++){
+                LCD_drawPixel(home-i,home,BLUE);
+            } 
+        }
+
         // Print data
-        sprintf(buff,"g: %d, %d, %d\r\n",data[1],data[2],data[3]);
-        drawString(20,30,WHITE,buff);
-        sprintf(buff,"a: %d, %d, %d",data[4],data[5],data[6]);
-        drawString(20,40,WHITE,buff);
-        core_delay(0.07);
-        
+        // sprintf(buff,"g: %d, %d, %d\r\n",data[1],data[2],data[3]);
+        // drawString(20,30,WHITE,buff);
+        // sprintf(buff,"a: %d, %d, %d",data[4],data[5],data[6]);
+        // drawString(20,40,WHITE,buff);
+
         // Clear last data 
-        sprintf(buff,"g: %d, %d, %d\r\n",data[1],data[2],data[3]);
-        drawString(20,30,BLUE,buff);
-        sprintf(buff,"a: %d, %d, %d",data[4],data[5],data[6]);
-        drawString(20,40,BLUE,buff);
+        // sprintf(buff,"g: %d, %d, %d\r\n",data[1],data[2],data[3]);
+        // drawString(20,30,BLUE,buff);
+        // sprintf(buff,"a: %d, %d, %d",data[4],data[5],data[6]);
+        // drawString(20,40,BLUE,buff);
+        
         NM32_LED1 = !NM32_LED1;
         
     }
